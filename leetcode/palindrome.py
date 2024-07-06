027d4d288757470fd81e3953925bc0f927a3b3db
archanajjoshi <archanajjoshi@gmail.com>
2024-07-06 18:25:22 -0300

    KAN-5: Add if statement

--- leetcode/palindrome.py
+++ leetcode/palindrome.py
@@ -1,8 +1,13 @@
-class palindromeSolution:
-    def palindrome(self, x) -> bool:
-        pal = str(x)
-        for i in (range((len(pal)/2) + (len(pal)%2)):
-                  j = len(pal)-1-i
-                  if pal[i] != pal[j]:
-                    return False
+def isPalindrome(x) -> bool:
+    palindrome_check = str(x)
+    length = len(palindrome_check)
+    if length >1:
+        for i in range(int(length / 2 + length % 2)):
+            j = len(palindrome_check) - 1 - i
+            if (palindrome_check[i] != palindrome_check[j]):
+                return False
         return True
+    else:
+        return False
+
+print(isPalindrome("m"))

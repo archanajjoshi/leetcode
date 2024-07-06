d810c76a79437454a3c23489fd63dfb72c1e8037
ArchanaQA <47013624+archanajjoshi@users.noreply.github.com>
2024-07-06 17:46:48 -0300

    Update easy_two_sum.py

--- leetcode/easy_two_sum.py
+++ leetcode/easy_two_sum.py
@@ -6,35 +6,32 @@ class Solution:
                 if target == nums[i] + nums[j]:
                     return [i,j]
 
-```Given an integer x, return true if x is a
-palindrome
-, and false otherwise.
 
- 
 
-Example 1:
 
-Input: x = 121
-Output: true
-Explanation: 121 reads as 121 from left to right and from right to left.
 
-Example 2:
+#Given an integer x, return true if x is a palindrome and false otherwise.
 
-Input: x = -121
-Output: false
-Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+#Example 1:
 
-Example 3:
+#Input: x = 121
+#Output: true
+#Explanation: 121 reads as 121 from left to right and from right to left.
 
-Input: x = 10
-Output: false
-Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+#Example 2:
 
- 
+#Input: x = -121
+#Output: false
+#Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
 
-Constraints:
+#Example 3:
 
-    -231 <= x <= 231 - 1
+#Input: x = 10
+#Output: false
+#Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
- 
-Follow up: Could you solve it without converting the integer to a string?```
+#Constraints:
+
+#-231 <= x <= 231 - 1
+
+#Follow up: Could you solve it without converting the integer to a string?```
